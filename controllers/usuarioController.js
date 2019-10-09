37acb005a2ef817af724a8031b67649a90bcc353
luisdario71196 <luisdario500@gmail.com>
2019-10-09 01:23:27 -0600

    Creacion de la ruta de los usuarios y el controller

---
+++ controllers/usuarioController.js
@@ -0,0 +1,17 @@
+// Importar Mogoose
+const mogoose = require("mongoose");
+const Usuario = mogoose.model("Usuario");
+
+// Crear un nuevo usuario
+exports.formularioNuevoUsuario = (req, res) => {
+  res.render("crearUsuario", {
+    nombrePagina: "Nuevo usuario"
+  });
+};
+
+// Agregar un nuevo usuario a la base de datos
+exports.agregarUsuario = async (req, res) => {
+  const usuario = new Usuario(req.body);
+  const nuevoUsuario = await usuario.save();
+  res.redirec("/nuevo/usuario");
+};
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,18 @@
+// Importar express
 const express = require("express");
+// Importar router
 const router = express.Router();
+// Importar los Controller
 const layoutController = require("../controllers/layoutController");
 const loginController = require("../controllers/loginController");
+const usuarioController = require("../controllers/usuarioController");
+// Rutas
 module.exports = () => {
   router.get("/", layoutController.mostrarMenu);
   router.get("/usuario/iniciarSesion", loginController.iniciarLogin);
+
+  router.get("/nuevo/usuario", usuarioController.formularioNuevoUsuario);
+  router.post("/nuevo/usuario", usuarioController.agregarUsuario);
 
   return router;
 };
